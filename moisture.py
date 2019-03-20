f218f9ff88dc72d85f411ab9159dbf61367ffd94
jonatello <ashevillainy@gmail.com>
2019-03-19 20:29:39 -0400

    Update moisture.py

--- moisture.py
+++ moisture.py
@@ -2,32 +2,17 @@
 
 # Edited from this original - https://github.com/modmypi/Moisture-Sensor/blob/master/moisture.py
 
-# Start by importing the libraries we want to use
-
-import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
-
-# This is our callback function, this function will be called every time there is a change on the specified GPIO channel
-
-def callback(channel):  
-	if GPIO.input(channel):
-		print "LED off"
-	else:
-		print "LED on"
+# Import the GPIO library
+import RPi.GPIO as GPIO
 
 # Set our GPIO numbering to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pin that we have our digital output from our sensor connected to
-channel = 21
+DO = 21
+
 # Set the GPIO pin to an input
-GPIO.setup(channel, GPIO.IN)
+GPIO.setup(DO, GPIO.IN)
 
-# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
-# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
-GPIO.add_event_callback(channel, callback)
-
-# This is an infinte loop to keep our script running
-while True:
-	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
-	time.sleep(0.1)
+# Print the value from the GPIO pin (1 is off, 0 is on/moisture)
+print 'Value: ', GPIO.input(DO)
