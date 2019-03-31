14521ce637ff8ffd9085936454a13b1f51a9b526
jonatello <ashevillainy@gmail.com>
2019-03-31 19:13:15 -0400

    Update and rename power_off.py to power_on.py

--- power_on.py
+++ power_on.py
@@ -10,16 +10,15 @@ timestamp = time.strftime("%x") + " " + time.strftime("%X")
 # Set our GPIO numbering to BCM
 GPIO.setmode(GPIO.BCM)
 
-# Define the GPIO pin that we have our digital output from our sensor connected to, 18 in this case
-DO = 18
+# Define the GPIO pin that we have our digital output from our sensor connected to, 24 in this case
+DO = 24
+
+# Suppress warnings on reuse of GPIO pin
+GPIO.setwarnings(False)
 
 # Set the GPIO pin to an output
 GPIO.setup(DO,GPIO.OUT)
 
 # Print for logging and turn on
-# print timestamp, "Value:  1"
-# GPIO.output(DO,GPIO.HIGH)
-
-# Print for logging and turn off
 print timestamp, "Value:  0"
 GPIO.output(DO,GPIO.LOW)
