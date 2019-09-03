f21a7b851727cdad98b33bfc303c47d901c83e22
Nick Rodriguez <Nick.Rodriguez@dhg.com>
2019-09-03 16:48:29 -0400

    Add Docker folder to build custom image.

---
+++ Docker/scripts/moisture.py
@@ -0,0 +1,11 @@
+import config
+import time
+import RPi.GPIO as GPIO
+
+timestamp = time.strftime("%x") + " " + time.strftime("%X")
+
+pin = config.moisture_pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN)
+
+print(timestamp, 'Value: ', GPIO.input(pin))
---
+++ Docker/scripts/outside.py
@@ -0,0 +1,15 @@
+import config
+import json
+import time
+import requests
+
+timestamp = time.strftime("%x") + " " + time.strftime("%X")
+
+OPENWEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?id=' + config.ow_city_id + '&APPID=' + config.ow_api_key + '&units=imperial'
+
+resp = requests.get(OPENWEATHER_URL)
+if resp.ok:
+    response = json.loads(resp.text)
+    temperature = response['main']['temp']
+    humidity = response['main']['humidity'] * .01
+    print(timestamp, 'Temp: ', temperature, 'Humidity: ', humidity)
