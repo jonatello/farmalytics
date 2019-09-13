7c0040e21e9d1a85133180a69020e2efc66b02fe
Nick Rodriguez <nickrod518@gmail.com>
2019-09-13 13:05:15 -0400

    Set logstash host via env var using sed in start script.

--- Docker/RPi-Node/farmalytics/scripts/start.sh
+++ Docker/RPi-Node/farmalytics/scripts/start.sh
@@ -7,5 +7,8 @@ cron
 printenv | sed 's/^\(.*\)$/export \1/g' > /farmalytics/scripts/load_env.sh
 chmod +x /farmalytics/scripts/load_env.sh
 
+# Update logstash host IP
+sed -i 's/<logstash_host_ip>/'"$LOGSTASH_HOST_IP"'/' filebeat.yml
+
 # Start filebeat
 /farmalytics/filebeat -e -c filebeat.yml
