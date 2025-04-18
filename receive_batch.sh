825d85e88b28a17d41d95ca8b35ba2bdd28d982c
jonatello <ashevillainy@gmail.com>
2025-04-18 14:19:04 -0400

    Create receive_batch.sh

---
+++ receive_batch.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+source meshtastic/bin/activate
+
+# Listen for messages from a specific node for 5 minutes
+python3 meshtastic_listener.py --sender_node_id "6209a0bd" --run_time 5
+
+# Format received messages into single message
+python3 meshtastic_input.py received_messages.log
+
+# Decode and unzip the jpg
+decodebase64_unzip_jpeg.sh
+
+deactivate
