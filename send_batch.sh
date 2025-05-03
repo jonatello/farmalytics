4c5478ef35bf9cb844f23d44264de374c8dd1d56
jonatello <ashevillainy@gmail.com>
2025-05-03 08:18:15 -0400

    Update send_batch.sh

--- send_batch.sh
+++ send_batch.sh
@@ -4,6 +4,6 @@ source meshtastic/bin/activate
 
 ./optimize_compress_zip_base64encode_jpg.sh 80 400x300
 
-python3 meshtastic_send.py base64_image.gz --dest '!47a78d36' --chunk_size 120
+python3 meshtastic_send.py base64_image.gz --connection tcp --dest '!47a78d36' --chunk_size 180
 
 deactivate
